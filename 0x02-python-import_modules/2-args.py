8c5cc2f126bb331e1832be2c8f739452bfb2a338
christopherchoe <chchchoe@gmail.com>
2018-12-05 20:18:03 +0000

    fixing import issues

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-from sys import argv
-if len(argv) == 1:
-    print('0 arguments.')
-else:
-    index = 1
-    if len(argv) == 2:
-        print('1 argument:')
+if __name__ == "__main__":
+    from sys import argv
+    if len(argv) == 1:
+        print('0 arguments.')
     else:
-        print('{} arguments:'.format(len(argv) - 1))
-    for i in argv[1:]:
-        print('{}: {}'.format(index, i))
-        index += 1
+        index = 1
+        if len(argv) == 2:
+            print('1 argument:')
+        else:
+            print('{} arguments:'.format(len(argv) - 1))
+        for i in argv[1:]:
+            print('{}: {}'.format(index, i))
+            index += 1
