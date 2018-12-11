ea0c1ba2bcbbe48e15d73369cdeb9faad1cad477
christopherchoe <chchchoe@gmail.com>
2018-12-11 20:55:31 +0000

    checking input type

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     max_int = 0
-    if len(my_list) == 0:
+    if type(my_list) is not list or len(my_list) == 0:
         return None
     for i in my_list:
         if i > max_int:
