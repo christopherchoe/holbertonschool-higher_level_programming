57ad26c7671e507ce0878eed8189f40ef38ecf76
christopherchoe <chchchoe@gmail.com>
2018-12-11 20:31:02 +0000

    fixed string format

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for row in range(len(matrix)):
         for column in range(len(matrix[row])):
-            print('{}'.format(matrix[row][column]), end='')
+            print('{:d}'.format(matrix[row][column]), end='')
             if column + 1 < len(matrix[row]):
-                print(' ', end='')
-        print('')
+                print(' '.format(), end='')
+        print(''.format())
