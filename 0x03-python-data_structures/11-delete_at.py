c2b5a773b580760c8fe7140899176dd75cfb6495
christopherchoe <chchchoe@gmail.com>
2018-12-11 21:29:42 +0000

    fixing return issue

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def delete_at(my_list=[], idx = 0):
+def delete_at(my_list=[], idx=0):
     if type(my_list) is not list:
         return
     if idx < 0 or idx >= len(my_list):
-        return
+        return my_list
     new_list = my_list[:idx] + my_list[idx + 1:]
     my_list.clear()
     for i in new_list:
