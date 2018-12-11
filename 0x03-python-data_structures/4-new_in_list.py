4f6187560a0a0c40bd31c7eb820ac9717646d03d
christopherchoe <chchchoe@gmail.com>
2018-12-11 18:31:59 +0000

    first draft

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    copy = my_list.copy()
+    if idx < 0:
+        return copy
+    if idx > len(my_list):
+        return copy
+    my_list[idx] = element
+    return copy
