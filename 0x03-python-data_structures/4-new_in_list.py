b90b56ffb17e77100fad55b1bdbfc8a269184af0
christopherchoe <chchchoe@gmail.com>
2018-12-11 20:43:35 +0000

    fixing list checking issue

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    copy = my_list.copy()
+    if type(my_list) is list:
+        copy = my_list.copy()
+    else:
+        return
     if idx < 0:
         return copy
     if idx >= len(my_list):
