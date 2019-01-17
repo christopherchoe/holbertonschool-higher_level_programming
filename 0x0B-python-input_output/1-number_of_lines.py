698c1893f11efb29c9a58932ddf464835ff81f92
christopherchoe <chchchoe@gmail.com>
2019-01-17 18:54:44 +0000

    initial commit

---
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def number_of_lines(filename=""):
+    """
+    returns number of lines in a text file
+    """
+    count = 0
+    with open(filename) as f:
+        for line in f:
+            count += 1
+    return count
