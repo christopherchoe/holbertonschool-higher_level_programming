bda28e7303d53400460b04707cb424e20d633824
christopherchoe <chchchoe@gmail.com>
2019-01-18 00:13:33 +0000

    initial commit

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+defines a class Student
+"""
+
+
+class Student():
+    """
+    student with name and age
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        initialization
+        """
+        if isinstance(first_name, str):
+            self.first_name = first_name
+        if isinstance(last_name, str):
+            self.last_name = last_name
+        if type(age) == int:
+            self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        retrieves dictionary representation of Student instance
+        """
+        if type(attrs) == list:
+            for i in attrs:
+                if type(i) != str:
+                    return self.__dict__
+            copy_dict = {}
+            for k, v in self.__dict__.items():
+                if k in attrs:
+                    copy_dict[k] = v
+            return copy_dict
+        return self.__dict__
