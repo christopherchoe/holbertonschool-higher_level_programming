fcc6fa30d49ea27cf96da3ac99f086629a6ab833
christopherchoe <chchchoe@gmail.com>
2019-03-04 23:05:19 +0000

    fixed output

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,5 @@
 -- displays average temperature by city ordrered by temperature
-SELECT city, AVG(SUM(*)) AS avg_temp
+SELECT city, AVG(value) AS avg_temp
 FROM temperatures
 GROUP BY city
 ORDER BY avg_temp DESC
