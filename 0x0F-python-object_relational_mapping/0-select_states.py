362e1cd981a8a90ad96b2e1b8373a6784ac70da1
christopherchoe <chchchoe@gmail.com>
2019-03-28 23:48:19 +0000

    fixed pe8 issues

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,12 +9,13 @@ if __name__ == "__main__":
     import sqlalchemy
     from sys import argv
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(
+        host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
 
     cur.execute("SELECT * FROM states ORDER BY states.id")
     rows = cur.fetchall()
     for row in rows:
-        print (row)
+        print(row)
     db.close()
