67e0580ce865af58e5f67f9ebbb68c04fbb0bf83
christopherchoe <chchchoe@gmail.com>
2019-03-29 16:16:16 +0000

    fixed underscore issue

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,6 +5,7 @@ class definition for state
 
 
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 import MySQLdb
 
 
@@ -15,8 +16,7 @@ class State(Base):
     """
     inherits from Base, states table object
     """
-
-    __table_name__ = 'states'
+    __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
