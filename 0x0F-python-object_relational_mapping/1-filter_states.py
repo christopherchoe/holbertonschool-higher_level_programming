9aa02adeea14a9015476e28a87b517a6092798c9
christopherchoe <chchchoe@gmail.com>
2019-03-29 16:32:46 +0000

    fixed line length issue

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -15,7 +15,9 @@ if __name__ == "__main__":
     cur = db.cursor()
 
     cur.execute(
-        "SELECT * FROM states WHERE BINARY states.name LIKE 'N%' ORDER BY states.id")
+        "SELECT * FROM states\
+        WHERE BINARY states.name\
+        LIKE 'N%' ORDER BY states.id")
     rows = cur.fetchall()
     for row in rows:
         print(row)
