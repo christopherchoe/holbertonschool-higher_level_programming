17f4c84a06ca511896e02e4fb392309173dcb1ef
christopherchoe <chchchoe@gmail.com>
2019-03-29 19:57:44 +0000

    fixed pep8 issue

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -19,7 +19,8 @@ if __name__ == "__main__":
     session = Session()
 
     try:
-        session.query(State).filter(State.id==2).update({"name": "New Mexico"})
+        session.query(State).filter(
+            State.id == 2).update({"name": "New Mexico"})
     except:
         pass
 
