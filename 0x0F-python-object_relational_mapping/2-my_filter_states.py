7bc41ecb04ccf15c59de3ab150238fcd6d431158
christopherchoe <chchchoe@gmail.com>
2019-03-29 01:04:13 +0000

    initial commit

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+script to list all states from hbtn_0e_0_usa matching argument
+"""
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sqlalchemy
+    from sys import argv
+
+    db = MySQLdb.connect(
+        host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+
+    cur = db.cursor()
+
+    cur.execute(
+        "SELECT * FROM states WHERE states.name = '{}' ORDER BY \
+        states.id".format(argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    db.close()
