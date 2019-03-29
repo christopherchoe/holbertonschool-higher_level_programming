de6dfb62fb6d22aa8d47cf05fc050c28c6e3de45
christopherchoe <chchchoe@gmail.com>
2019-03-29 22:30:05 +0000

    used backref for relationship

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -19,4 +19,4 @@ class City(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
-    state = relationship("State", back_populates="children")
+    state = relationship("State", backref="children")
