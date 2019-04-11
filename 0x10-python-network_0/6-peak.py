e3e7df2f4a0453644d2c611769a3bbbc9342d616
christopherchoe <chchchoe@gmail.com>
2019-04-11 06:43:06 +0000

    initial commit

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+peak finder
+"""
+
+
+def find_peak(list_of_integers):
+    """
+    finds any peak
+    """
+    length = len(list_of_integers)
+    if length > 0:
+        return re_peak(list_of_integers, length)
+
+
+def re_peak(list_of_integers, length):
+    """
+    finds peak using recursion
+    """
+    if length > 1:
+        if list_of_integers[1] > list_of_integers[0]:
+            if (length > 2 and list_of_integers[1] > list_of_integers[2]) or length == 2:
+                return list_of_integers[1]
+            else:
+                return re_peak(list_of_integers[2:], length - 2)
+        return list_of_integers[0]
+    else:
+        return list_of_integers[0]
