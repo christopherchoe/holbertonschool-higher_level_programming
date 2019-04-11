ef03d9e59ff6c942d2a6b0523260d1825f454a00
christopherchoe <chchchoe@gmail.com>
2019-04-11 06:56:19 +0000

    made into true logn time complexity

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -9,14 +9,18 @@ def find_peak(list_of_integers):
     finds any peak
     """
     length = len(list_of_integers)
+    half = length // 2
     if length > 0:
         if length > 1:
-            if list_of_integers[1] > list_of_integers[0]:
-                if (length > 2 and list_of_integers[1] > list_of_integers[2])\
+            if list_of_integers[half] > list_of_integers[half - 1]:
+                if (length > 2 and list_of_integers[half] > list_of_integers[half + 1])\
                         or length == 2:
-                    return list_of_integers[1]
+                    return list_of_integers[half]
                 else:
-                    return find_peak(list_of_integers[2:])
-            return list_of_integers[0]
+                    ret = find_peak(list_of_integers[half:])
+                    if ret is None:
+                        ret = find_peak(list_of_integers[:half])
+                    return ret
+            return list_of_integers[half - 1]
         else:
             return list_of_integers[0]
