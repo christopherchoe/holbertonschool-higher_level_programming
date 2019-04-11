85c990bbb71e98c59a292976a49c3ac6fa779b39
christopherchoe <chchchoe@gmail.com>
2019-04-11 06:49:19 +0000

    made into one function

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -10,20 +10,13 @@ def find_peak(list_of_integers):
     """
     length = len(list_of_integers)
     if length > 0:
-        return re_peak(list_of_integers, length)
-
-
-def re_peak(list_of_integers, length):
-    """
-    finds peak using recursion
-    """
-    if length > 1:
-        if list_of_integers[1] > list_of_integers[0]:
-            if (length > 2 and list_of_integers[1] > list_of_integers[2])\
-                    or length == 2:
-                return list_of_integers[1]
-            else:
-                return re_peak(list_of_integers[2:], length - 2)
-        return list_of_integers[0]
-    else:
-        return list_of_integers[0]
+        if length > 1:
+            if list_of_integers[1] > list_of_integers[0]:
+                if (length > 2 and list_of_integers[1] > list_of_integers[2])\
+                        or length == 2:
+                    return list_of_integers[1]
+                else:
+                    return find_peak(list_of_integers[2:])
+            return list_of_integers[0]
+        else:
+            return list_of_integers[0]
