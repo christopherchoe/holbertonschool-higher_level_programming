82c29445128ea903c151e4d431a0d53ba8be38af
christopherchoe <chchchoe@gmail.com>
2019-04-11 06:46:13 +0000

    changed pep8 and time complexity

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -19,7 +19,8 @@ def re_peak(list_of_integers, length):
     """
     if length > 1:
         if list_of_integers[1] > list_of_integers[0]:
-            if (length > 2 and list_of_integers[1] > list_of_integers[2]) or length == 2:
+            if (length > 2 and list_of_integers[1] > list_of_integers[2])\
+                    or length == 2:
                 return list_of_integers[1]
             else:
                 return re_peak(list_of_integers[2:], length - 2)
