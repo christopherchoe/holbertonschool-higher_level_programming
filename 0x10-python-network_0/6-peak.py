d6dd6c83584dbc82397005cca4fe62eb754a5b7c
christopherchoe <chchchoe@gmail.com>
2019-04-11 07:09:47 +0000

    small fixes to logic

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -12,14 +12,17 @@ def find_peak(list_of_integers):
     half = length // 2
     if length > 0:
         if length > 1:
+            if length == 2:
+                return list_of_integers[0] if\
+                    list_of_integers[0] > list_of_integers[1] else\
+                    list_of_integers[1]
             if list_of_integers[half] > list_of_integers[half - 1]:
-                if (length > 2 and list_of_integers[half] > list_of_integers[half + 1])\
-                        or length == 2:
+                if list_of_integers[half] > list_of_integers[half + 1]:
                     return list_of_integers[half]
                 else:
-                    ret = find_peak(list_of_integers[half:])
+                    ret = find_peak(list_of_integers[half + 1:])
                     if ret is None:
-                        ret = find_peak(list_of_integers[:half])
+                        ret = find_peak(list_of_integers[:-half])
                     return ret
             return list_of_integers[half - 1]
         else:
