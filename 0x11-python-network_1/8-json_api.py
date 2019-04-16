8346b424b9fc61e6bbda698604bfaa2d493816e8
christopherchoe <chchchoe@gmail.com>
2019-04-16 05:05:34 +0000

    initial commit

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+script to takes url and sends post request with input letter
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    param = {'q': ""}
+    if len(sys.argv) > 1:
+        param['q'] = sys.argv[1]
+    r = requests.post('http://0.0.0.0:5000/search_user', data=param)
+    try:
+        if r.json:
+            print('[{}] {}'.format(r.json()['id'], r.json()['name']))
+        else:
+            print('No result')
+    except ValueError:
+        print('Not a valid JSON')
