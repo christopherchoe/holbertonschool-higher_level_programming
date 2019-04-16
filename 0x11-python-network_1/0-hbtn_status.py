90f47c8c8603bc88ebd83624539a313de6180385
christopherchoe <chchchoe@gmail.com>
2019-04-16 01:08:11 +0000

    changed tabulation

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,6 +8,6 @@ import urllib.request
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
     read = response.read()
     print('Body response:')
-    print('     - type: {}'.format(type(read)))
-    print('     - content: {}'.format(read))
-    print('     - utf8 content: {}'.format(read.decode('utf-8')))
+    print('\t- type: {}'.format(type(read)))
+    print('\t- content: {}'.format(read))
+    print('\t- utf8 content: {}'.format(read.decode('utf-8')))
