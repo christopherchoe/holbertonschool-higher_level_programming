2ceb1c92212d84e75004e57e98d7e4155d5191b5
christopherchoe <chchchoe@gmail.com>
2019-04-16 04:23:31 +0000

    fixed error handling

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,8 +7,11 @@ import sys
 
 
 if __name__ == "__main__":
-    r = requests.get(sys.argv[1])
-    if r.raise_for_status == requests.codes.ok:
-        print(r.text)
-    else:
-        print('Error code: {}'.format(r.status_code))
+    try:
+        r = requests.get(sys.argv[1])
+        if r.raise_for_status == requests.codes.ok:
+            print(r.text)
+        else:
+            print('Error code: {}'.format(r.status_code))
+    except:
+        pass
