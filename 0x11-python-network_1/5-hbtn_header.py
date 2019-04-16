e670b76524da3da6f73eafe43f266a589b0c7237
christopherchoe <chchchoe@gmail.com>
2019-04-16 02:58:43 +0000

    initial commit

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+script to takes url and sends request
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    r = requests.get(sys.argv[1])
+    print(r.headers['X-Request-Id'])
