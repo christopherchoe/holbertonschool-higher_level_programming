876f04ab77b1f8df0d72f3e981ad1f78c724dd9b
christopherchoe <chchchoe@gmail.com>
2019-04-16 06:00:36 +0000

    initial commit

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+script to search github api
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = 'https://api.github.com/user'
+    r = requests.get(url, auth=(sys.argv[1], sys.argv[2]))
+    try:
+        di = r.json()
+        if 'id' in di.items():
+            print(di['id'])
+        else:
+            print('None')
+    except:
+        pass
