d5681c9d521a560565ee6dccf0004e1f661e01cd
christopherchoe <chchchoe@gmail.com>
2019-04-16 04:29:05 +0000

    fixed status code 200 issue

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,7 +9,7 @@ import sys
 if __name__ == "__main__":
     try:
         r = requests.get(sys.argv[1])
-        if r.raise_for_status == requests.codes.ok:
+        if r.status_code == 200:
             print(r.text)
         else:
             print('Error code: {}'.format(r.status_code))
