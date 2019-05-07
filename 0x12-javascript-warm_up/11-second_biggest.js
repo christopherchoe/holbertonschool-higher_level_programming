1d53465249b5737e7a1963ab086f7c9ada860985
christopherchoe <chchchoe@gmail.com>
2019-05-07 21:17:41 +0000

    fixed second setting issue with non maxes

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -8,8 +8,11 @@ if (process.argv[2] === undefined || process.argv[3] === undefined) {
     : parseInt(process.argv[2]);
   process.argv.forEach((val, index) => {
     if (parseInt(val) > max) {
+      console.log('test' + second + ' ' + max);
       second = max;
       max = parseInt(val);
+    } else if (parseInt(val) !== max && parseInt(val) > second) {
+      second = parseInt(val);
     }
   });
   console.log(second);
