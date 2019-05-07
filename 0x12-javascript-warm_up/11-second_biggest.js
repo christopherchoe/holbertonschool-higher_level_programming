6f7727e1c4638d9ebd4cdc7ebbe6efa8428cb2fe
christopherchoe <chchchoe@gmail.com>
2019-05-07 21:10:56 +0000

    fixed initial second set issue

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,9 @@ if (process.argv[2] === undefined || process.argv[3] === undefined) {
   console.log('0');
 } else {
   let max = parseInt(process.argv[2]);
-  let second = parseInt(process.argv[2]);
+  let second = parseInt(process.argv[2]) > parseInt(process.argv[3])
+    ? parseInt(process.argv[3])
+    : parseInt(process.argv[2]);
   process.argv.forEach((val, index) => {
     if (parseInt(val) > max) {
       second = max;
