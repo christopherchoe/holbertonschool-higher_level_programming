de5be56d05bde67efa4158357880dfc6af735266
christopherchoe <chchchoe@gmail.com>
2019-05-14 22:18:22 +0000

    changed logic to not add userid if not completed

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -7,7 +7,7 @@ request(process.argv[2], function (error, response, body) {
   } else {
     let taskList = JSON.parse(response.body);
     for (let items in taskList) {
-      if (!(tasks[taskList[items]['userId']])) {
+      if (!(tasks[taskList[items]['userId']]) && taskList[items]['completed'] === true) {
         tasks[taskList[items]['userId']] = 0;
       }
       if (taskList[items]['completed'] === true) {
