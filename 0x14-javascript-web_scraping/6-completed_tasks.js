df35a51f306ec4a88a3f6748893427771566a441
christopherchoe <chchchoe@gmail.com>
2019-05-14 21:58:09 +0000

    initial 6 commit

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const request = require('request');
+let tasks = {};
+request(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+  let taskList = JSON.parse(body);
+  for (let items in taskList) {
+    if (tasks[taskList[items]['userId']] === undefined) {
+      tasks[taskList[items]['userId']] = 0;
+    }
+    if (taskList[items]['completed'] === true) {
+      tasks[taskList[items]['userId']] += 1;
+    }
+  }
+  console.log(tasks);
+});
